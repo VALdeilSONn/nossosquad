41250e3f91a597b552bc4b94e0b6d0f08cd470b8
VALdeilSONn <86812071+VALdeilSONn@users.noreply.github.com>
2022-10-28 21:36:55 -0300

    Add files via upload

--- js/lista.js
+++ js/lista.js
@@ -1,49 +1,25 @@
-
 document.querySelector('#crc').addEventListener('click', (e) => {
     e.preventDefault();
-        
-     if(document.querySelector('#nome').value.length == 0){
+
+    if (document.querySelector('#nome').value.length == 0) {
         swal("Por favor digite seu nome");
-        
-     }else if(document.querySelector('#tel').value.length == 0){
+
+    } else if (document.querySelector('#tel').value.length == 0) {
         swal("Por favor digite seu telefone");
-       
-     }else if(document.querySelector('#mail').value.length == 0){
+
+    } else if (document.querySelector('#mail').value.length == 0) {
         swal("Por favor digite seu e-mail");
-        
-     }else if(document.querySelector('#mensagem').value.length == 0){ 
+
+    } else if (document.querySelector('#mensagem').value.length == 0) {
         swal("Por favor digite sua mensagem");
-       
 
-    }else{
+
+    } else {
         swal("Obrigado, entraremos em contato em breve!");
-        
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        window.location.href = "obrigado.html";
 
     }
 
@@ -52,7 +28,7 @@ document.querySelector('#crc').addEventListener('click', (e) => {
 });
 
 
-  
+
 
 
 
@@ -82,5 +58,5 @@ document.querySelector('#crc').addEventListener('click', (e) => {
 //var btnenv = document.getElementById('crc');
 //btnenv.addEventListener('click', () =>{
 
-   // swal("obrigado pelo contato, em breve entraremos em contato");
+// swal("obrigado pelo contato, em breve entraremos em contato");
 //}
